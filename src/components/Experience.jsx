fabf331bac221dafe5a0f82977ac8bb2b69228f1
Jake2508 <72077965+Jake2508@users.noreply.github.com>
2025-05-24 11:37:43 +0100

    Updated Hover Effect Logic
    - Added hover outline and grow effect
    - Imported React Spring for smooth transitions
    - Updated model name

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,14 @@
 // Core Extensions
 import { Stars, GradientTexture, Environment, useGLTF, OrbitControls } from '@react-three/drei';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useRef } from 'react';
 
 // Post Processing Effects
-import { ToneMapping, EffectComposer, DepthOfField, Vignette } from '@react-three/postprocessing';
+import { Selection, Select, Outline, ToneMapping, EffectComposer, Vignette } from '@react-three/postprocessing';
 import { ToneMappingMode, BlendFunction } from 'postprocessing';
 
 // Custom Hooks & Components
 import { useHover } from '../hooks/useHover.js'; 
+import Interactable from './Interactable.js';
 
 // Entities
 import WinterEnvironment from '../entities/WinterEnvironment.js';
@@ -17,11 +18,14 @@ import { Perf } from 'r3f-perf';
 
 
 export default function Experience({ onSelectProject, isVisible }) {
-    // Setup Model
-    const arcadeMachine = useMemo(() => useGLTF('/Models/ArcadeMachine.gltf'), []);
+    
+    // Static Scene
+    const staticScene = useMemo(() => useGLTF('/Models/WinterScene.gltf'), []);
 
-    // Hover Effect Hook
-    const { handlePointerOver, handlePointerOut, hoveredObject } = useHover(); 
+    // Effects, Hooks & Refs
+    const [hovered, setHovered] = useState(false); 
+    const orbitRef = useRef();
+
 
     const handleArcadeClick = () => {
         onSelectProject({
@@ -30,7 +34,7 @@ export default function Experience({ onSelectProject, isVisible }) {
         });
     };
 
-    // Memoize Environment, Lighting & Post-Processing effects to render once (Static Scene)
+    // Memoize Environment
     const environment = useMemo(() => (
         <>
             <Environment preset="forest" />
@@ -43,19 +47,11 @@ export default function Experience({ onSelectProject, isVisible }) {
         </>
     ), []);
 
-    const postProcessingEffects = useMemo(() => {
-        return (
-            <EffectComposer multisampling={2} >
-                <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />  
-                <Vignette offset={0.2} darkness={0.45} blendFunction={BlendFunction.COLOR_DODGE} />
-            </EffectComposer>
-            );
-        }, []); // Only recalculated once
         
     return (
         <>
             {/* Perf component monitors performance */}
-            {/* <Perf position="top-left" /> */}
+            <Perf position="top-left" />
 
             {/* Environment, Lighting & Background */}
             {environment}
@@ -68,33 +64,40 @@ export default function Experience({ onSelectProject, isVisible }) {
                 fade speed={0.75} 
             />
 
-            {/* Post Processing */}
-            {postProcessingEffects}
+            <Selection>
+                {/* Post Processing */}
+                <EffectComposer multisampling={8} autoClear={false}>
+                    <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />  
+                    <Vignette offset={0.2} darkness={0.45} blendFunction={BlendFunction.COLOR_DODGE} />
+                    <Outline visibleEdgeColor="white" edgeStrength={40} width={1000} />
+                </EffectComposer>
 
-            {/* Orbit Controls */}
-            <OrbitControls makeDefault enableDamping={true} dampingFactor={0.1} enablePan={false} 
-                minPolarAngle={Math.PI / 4.5} maxPolarAngle={Math.PI / 2.2}
-                minDistance={7.0} maxDistance={25} 
-                enabled={!isVisible}  
-                autoRotate={true} autoRotateSpeed={0.2}
-                // Mobile Support
-                touches={{ 
-                    ONE: 0, // Single-finger rotate
-                    TWO: 2, // Two-finger zoom
-                  }}
-            />
+                {/* Orbit Controls */}
+                <OrbitControls makeDefault enableDamping={true} dampingFactor={0.1} enablePan={false} 
+                    minPolarAngle={Math.PI / 4.5} maxPolarAngle={Math.PI / 2.2}
+                    minDistance={7.0} maxDistance={25} 
+                    enabled={!isVisible}  
+                    autoRotate={true} autoRotateSpeed={0.2}
+                    // Mobile Support
+                    touches={{ 
+                        ONE: 0, // Single-finger rotate
+                        TWO: 2, // Two-finger zoom
+                    }}
+                />
 
-            {/* Static Scene Objects */}
-            <WinterEnvironment />
+                {/* Static Scene Objects */}
+                <primitive object={staticScene.scene} scale={0.4} position-y={-1.4} castShadow={false} receiveShadow={false} />
 
-            {/* Arcade Machine */}
-            <primitive 
-                object={arcadeMachine.scene} scale={0.4} position-y={-1.4} 
-                castShadow={false} receiveShadow={false} 
-                onPointerOver={(event) => handlePointerOver(event, arcadeMachine.scene)} 
-                onPointerOut={(event) => handlePointerOut(event, arcadeMachine.scene)}
+                {/* Interactables */}
+                <Select enabled={hovered}>
+                    
+                {/* Arcade Machine */}
+                <Interactable modelName="ArcadeMachine" position-y={-1.4} castShadow={false} receiveShadow={false}
                 onClick={handleArcadeClick}
-            />
+                />
+
+                </Select>
+            </Selection>
         </>
     );
 }
